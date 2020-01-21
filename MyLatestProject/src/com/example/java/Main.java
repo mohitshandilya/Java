f788979ebdd1549ad03a1880b5044c01cab97ee5
mohitshandilya <shandilyamohit23@gmail.com>
2020-01-21 22:58:49 +0530

    All Java Practise files (old and new) - 21 Jan 2020.
    Practise will continue from today.

---
+++ MyLatestProject/src/com/example/java/Main.java
@@ -0,0 +1,10 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Welcome back to My Project");
+        String myName = "Mohit";
+        System.out.println("My name is "+ myName);
+    }
+}
---
+++ Packages/com/example/java/Main.java
@@ -0,0 +1,9 @@
+package com.example.java;
+
+public class Main {
+		
+		public static void main(String[] args) {
+				System.out.println("Yayyy!! I completed my first Java Program!!");
+				System.out.println(args[0]);
+		}
+}
